f29ceef1e3c82563e8b6ad9865cbf6b66e7aac48
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-23 15:14:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,11 @@ class App < Sinatra::Base
   post '/team' do 
     @name = params[:name]
     @coach = params[:coach]
-    @name = params[:name]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
     erb :team
   end
   
